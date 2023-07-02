a81725c4d75b7c4fab0fdf7b8aa8349d34e03306
romopjorge <romopena.jorge@gmail.com>
2023-07-02 05:09:39 -0400

    Codigo Raspberry Pi

---
+++ Proyecto/request_send_data.py
@@ -0,0 +1,26 @@
+import serial
+import requests
+
+url = "http://192.168.38.90/cgid-bin/first.py"
+
+class Xbee:
+    def __init__(self, port):
+        self.port = port
+        self.ser = serial.Serial(port, 9600)
+
+    def request_data(self, sta):
+        self.ser.write(sta)
+        data = self.ser.readline().decode().strip()
+        return data
+
+coord = Xbee('/dev/ttyUSB0')
+
+sta1 = coord.request_data(b'sta1')
+sta2 = coord.request_data(b'sta2')
+
+#print ('Data from Station 1:', sta1)
+#print ('Data from Station 2:', sta2)
+
+x = requests.post(url, data = sta1)
+x = requests.post(url, data = sta2)
+print (x.text)
